e4d9df2a3f455e9453d1b17b5fa9b64c2f090ff2
Geraldo Júnior <geraldo.junior0207@gmail.com>
2022-01-31 12:06:50 -0300

    implementação de busca inteligente (retorna ao inicio da sequencia)

--- src/controladores.js/traducao.js
+++ src/controladores.js/traducao.js
@@ -5,7 +5,7 @@ const dicionario = [['a', 'b', 'c'],['d', 'e', 'f'], ['g', 'h', 'i'],
 const traducao = async (req, res) => {
     const {codigo, shift} = req.body;
     const arrayNumbers = codigo.split('');
-    const pos = arrayNumbers.length - 1;
+    let pos = arrayNumbers.length - 1;
 
     if(!codigo || arrayNumbers.length === 0 || arrayNumbers[0] === "1") return res.status(400).json("Código inválido");
 
@@ -25,7 +25,11 @@ const traducao = async (req, res) => {
         return res.status(200).json(espacos);
     }
 
-    if(dicionario[parseInt(arrayNumbers[0]) - 2].length < pos + 1) return res.status(400).json("Código inválido");
+    
+
+    if(pos + 1 > dicionario[parseInt(arrayNumbers[0]) - 2].length) {
+        pos = (arrayNumbers.length % dicionario[parseInt(arrayNumbers[0]) - 2].length) - 1;
+    }
     const resposta = dicionario[parseInt(arrayNumbers[0]) - 2][pos];
     return res.status(200).json(shift ? resposta.toLocaleUpperCase() : resposta);
 
